f1dacd3484c7fdf630d73d448456f080e1eefe71
gbenedetti <gbenedetti.1990@gmail.com>
2018-07-09 17:30:52 -0300

    Added Post, Put Vue.Js examples, and swagger to the Kotlin REST Api

--- kotlin-demo/src/main/kotlin/com/example/kotlindemo/controller/ArticleController.kt
+++ kotlin-demo/src/main/kotlin/com/example/kotlindemo/controller/ArticleController.kt
@@ -5,7 +5,6 @@ import com.example.kotlindemo.repository.ArticleRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.util.*
 import javax.validation.Valid
 
 @RestController
